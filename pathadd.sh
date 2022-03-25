ad2e88b7c6fb144863b79e046ebaf75d8e0eb193
kaku10 <>
2022-03-25 19:16:49 +0900

    Add ffmpeg

---
+++ pathadd.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+PROJECT_DIR=$(cd $(dirname $0); pwd)
+
+export PATH="${PROJECT_DIR}/bin:${PATH}"
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+PROJECT_DIR=$(cd $(dirname $0); pwd)
+
+no_cache_flag=false
+
+local -A opthash
+zparseopts -D -A opthash -- -help -version v
+
+if [[ -n "${opthash[(i)--no-cache]}" ]]; then
+  no_cache_flag='true'
+fi
+
+docker build -t ffmpeg ${PROJECT_DIR}/ffmpeg --no-cache=${no_cache_flag}
