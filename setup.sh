7a7ece897d9bab83fe23e161c4d3dc4f616b61bd
kaku10 <>
2022-08-13 01:07:35 +0900

    Add jq

--- setup.sh
+++ setup.sh
@@ -13,3 +13,4 @@ fi
 
 docker build -t ffmpeg ${PROJECT_DIR}/ffmpeg --no-cache=${no_cache_flag}
 docker build -t sfdx ${PROJECT_DIR}/sfdx --no-cache=${no_cache_flag}
+docker build -t jq ${PROJECT_DIR}/jq --no-cache=${no_cache_flag}
