6c0bc9a8bdb8e29112bd48bedad8f641c85c875a
kaku10 <>
2022-08-13 15:50:08 +0900

    Lint all Dockerfiles

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+set -e
+
 PROJECT_DIR=$(cd $(dirname $0); pwd)
 
 no_cache_flag=false
@@ -10,6 +12,15 @@ zparseopts -D -A opthash -- -help -version v
 if [[ -n "${opthash[(i)--no-cache]}" ]]; then
   no_cache_flag='true'
 fi
+
+if [[ -n "${opthash[(i)--no-fail]}" ]]; then
+  set +e
+fi
+
+for dockerfile in ${PROJECT_DIR}/*/Dockerfile ; do
+  echo "Linting ${dockerfile}..."
+  docker run --rm -i hadolint/hadolint < "${dockerfile}"
+done
 
 docker build -t ffmpeg ${PROJECT_DIR}/ffmpeg --no-cache=${no_cache_flag}
 docker build -t sfdx ${PROJECT_DIR}/sfdx --no-cache=${no_cache_flag}
