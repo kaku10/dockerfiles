cf720dd5e5f1f7d39d7844404a17ab2abe3f0fe0
kaku10 <>
2022-08-13 16:18:32 +0900

    Add --lint flag

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ no_cache_flag=false
 local -A opthash
 zparseopts -D -A opthash -- -help -version v
 
+if [[ -n "${opthash[(i)--lint]}" ]]; then
+  lint_flag='true'
+fi
+
 if [[ -n "${opthash[(i)--no-cache]}" ]]; then
   no_cache_flag='true'
 fi
@@ -17,10 +21,12 @@ if [[ -n "${opthash[(i)--no-fail]}" ]]; then
   set +e
 fi
 
-for dockerfile in "${PROJECT_DIR}"/*/Dockerfile ; do
-  echo "Linting ${dockerfile}..."
-  docker run --rm -i hadolint/hadolint < "${dockerfile}"
-done
+if [ ${lint_flag} == 'true' ]; then
+  for dockerfile in "${PROJECT_DIR}"/*/Dockerfile ; do
+    echo "Linting ${dockerfile}..."
+    docker run --rm -i hadolint/hadolint hadolint --ignore DL3008 - < "${dockerfile}"
+  done
+fi
 
 docker build -t ffmpeg "${PROJECT_DIR}/ffmpeg" --no-cache=${no_cache_flag}
 docker build -t sfdx "${PROJECT_DIR}/sfdx" --no-cache=${no_cache_flag}
