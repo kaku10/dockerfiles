211a1de6afbccace1f9a72d7d1944bfa26acb3e2
kaku10 <>
2022-10-19 23:46:35 +0900

    Add golang

--- setup.sh
+++ setup.sh
@@ -42,6 +42,7 @@ if [ ${lint_flag} == 'true' ]; then
 fi
 
 docker build -t ffmpeg "${PROJECT_DIR}/ffmpeg" --no-cache=${no_cache_flag}
+docker build -t golang "${PROJECT_DIR}/golang" --no-cache=${no_cache_flag}
 docker build -t sfdx "${PROJECT_DIR}/sfdx" --no-cache=${no_cache_flag}
 docker build -t jq "${PROJECT_DIR}/jq" --no-cache=${no_cache_flag}
 docker build -t node "${PROJECT_DIR}/node" --no-cache=${no_cache_flag}
